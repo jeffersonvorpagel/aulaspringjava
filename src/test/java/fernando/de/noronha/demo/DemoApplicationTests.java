7cd9c955f08566ba0506abb414ada827e3c856a7
JEFFERSON DOS SANTOS VORPAGEL <jefferson.vorpagel@alunos.bpkedu.com.br>
2024-03-12 21:39:06 -0300

    primeiro commit

---
+++ src/test/java/fernando/de/noronha/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fernando.de.noronha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
